e313ca768dbbac24345b3d805f734677a120b204
Michał Kucal <michal.kucal@intive.com>
2023-05-18 10:32:29 +0200

    Initialize project

---
+++ WeatherForecast/WeatherForecast/WeatherForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecastApp.swift
+//  WeatherForecast
+//
+//  Created by Michał Kucal on 18/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
