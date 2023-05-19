d6d1a6126b206b5df321fce83376411d508daee1
Michał Kucal <michal.kucal@intive.com>
2023-05-19 17:13:50 +0200

    Mapping API model

--- WeatherForecast/WeatherForecast/Models/WeatherApiData.swift
+++ WeatherForecast/WeatherForecast/Models/WeatherApiData.swift
@@ -8,10 +8,20 @@
 import Foundation
 
 struct WeatherApiData: Decodable {
+    var currentWeather: CurrentWeatherApiData?
+    var hourlyForecastUnits: HourlyForecastUnitsApiData?
+    var hourlyForecast: HourlyForecastApiData?
+    var dailyForecastUnits: DailyForecastUnitsApiData?
+    var dailyForecast: DailyForecastApiData?
 
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
 
+        currentWeather = try container.decodeIfPresent(CurrentWeatherApiData.self, forKey: .currentWeather)
+        hourlyForecastUnits = try container.decodeIfPresent(HourlyForecastUnitsApiData.self, forKey: .hourlyForecastUnits)
+        hourlyForecast = try container.decodeIfPresent(HourlyForecastApiData.self, forKey: .hourlyForecast)
+        dailyForecastUnits = try container.decodeIfPresent(DailyForecastUnitsApiData.self, forKey: .dailyForecastUnits)
+        dailyForecast = try container.decodeIfPresent(DailyForecastApiData.self, forKey: .dailyForecast)
     }
 
     private enum CodingKeys: String, CodingKey {
@@ -24,10 +34,20 @@ struct WeatherApiData: Decodable {
 }
 
 struct CurrentWeatherApiData: Decodable {
+    var temperature: Double?
+    var windSpeed: Double?
+    var windDirection: Double?
+    var weatherCode: Int?
+    var time: Date?
 
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
 
+        temperature = try container.decodeIfPresent(Double.self, forKey: .temperature)
+        windSpeed = try container.decodeIfPresent(Double.self, forKey: .windSpeed)
+        windDirection = try container.decodeIfPresent(Double.self, forKey: .windDirection)
+        weatherCode = try container.decodeIfPresent(Int.self, forKey: .weatherCode)
+        // TODO: time = try container.decodeIfPresent(Double.self, forKey: .time)
     }
 
     private enum CodingKeys: String, CodingKey {
@@ -40,10 +60,26 @@ struct CurrentWeatherApiData: Decodable {
 }
 
 struct HourlyForecastUnitsApiData: Decodable {
+    var temperatureUnit: String?
+    var relativeHumidityUnit: String?
+    var apparentTemperatureUnit: String?
+    var precipitationUnit: String?
+    var weatherCodeUnit: String?
+    var surfacePressureUnit: String?
+    var windSpeedUnit: String?
+    var windDirectionUnit: String?
 
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
 
+        temperatureUnit = try container.decodeIfPresent(String.self, forKey: .temperature)
+        relativeHumidityUnit = try container.decodeIfPresent(String.self, forKey: .relativeHumidity)
+        apparentTemperatureUnit = try container.decodeIfPresent(String.self, forKey: .apparentTemperature)
+        precipitationUnit = try container.decodeIfPresent(String.self, forKey: .precipitation)
+        weatherCodeUnit = try container.decodeIfPresent(String.self, forKey: .weatherCode)
+        surfacePressureUnit = try container.decodeIfPresent(String.self, forKey: .surfacePressure)
+        windSpeedUnit = try container.decodeIfPresent(String.self, forKey: .windSpeed)
+        windDirectionUnit = try container.decodeIfPresent(String.self, forKey: .windDirection)
     }
 
     private enum CodingKeys: String, CodingKey {
