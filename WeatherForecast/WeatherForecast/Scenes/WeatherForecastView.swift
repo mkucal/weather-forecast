3feb3ab0856f0f04008c51e4a10b59396014b59b
Michał Kucal <michal.kucal@intive.com>
2023-05-20 23:53:18 +0200

    Updating UI

--- WeatherForecast/WeatherForecast/Scenes/WeatherForecastView.swift
+++ WeatherForecast/WeatherForecast/Scenes/WeatherForecastView.swift
@@ -15,9 +15,7 @@ struct WeatherForecastView: View {
 
     var body: some View {
         VStack {
-            HStack(spacing: 20) {
-                Spacer()
-
+            HStack {
                 Button(action: {
 
                 }) {
@@ -26,6 +24,8 @@ struct WeatherForecastView: View {
                         .frame(width: 40, height: 40)
                         .padding(.leading, 20)
                 }
+
+                Spacer()
 
                 Button(action: {
 
@@ -44,48 +44,111 @@ struct WeatherForecastView: View {
                 Text("Poznań")
                     .font(.title2)
                 Text("20℃")
-                    .font(.largeTitle)
+                    .font(.system(size: 50))
                 Image(systemName: "cloud")
-                    .font(.title3)
+                    .font(.title2)
                 Text("Partly cloudly")
                     .font(.callout)
             }
+            .padding(.bottom, 10)
 
             List {
                 Section("Hourly forecast") {
                     ScrollView(.horizontal) {
                         HStack {
-                            Text("First Row")
-                                .padding()
-                                .background(.white)
-                                .cornerRadius(8)
-                            Text("Second Row")
-                                .padding()
-                                .background(.white)
-                                .cornerRadius(8)
-                            Text("Third Row")
-                                .padding()
-                                .background(.white)
-                                .cornerRadius(8)
-                            Text("Fourth Row")
-                                .padding()
-                                .background(.white)
-                                .cornerRadius(8)
+                            VStack(spacing: 4) {
+                                Text("00:00")
+                                Image(systemName: "cloud")
+                                    .font(.title2)
+                                Text("20℃")
+                            }
+                            .padding(.vertical, 10)
+                            .frame(width: 90)
+                            .background(.gray)
+                            .cornerRadius(8)
+
+                            VStack(spacing: 4) {
+                                Text("00:00")
+                                Image(systemName: "cloud")
+                                    .font(.title2)
+                                Text("20℃")
+                            }
+                            .padding(.vertical, 10)
+                            .frame(width: 90)
+                            .background(.gray)
+                            .cornerRadius(8)
+
+                            VStack(spacing: 4) {
+                                Text("00:00")
+                                Image(systemName: "cloud")
+                                    .font(.title2)
+                                Text("20℃")
+                            }
+                            .padding(.vertical, 10)
+                            .frame(width: 90)
+                            .background(.gray)
+                            .cornerRadius(8)
+
+                            VStack(spacing: 4) {
+                                Text("00:00")
+                                Image(systemName: "cloud")
+                                    .font(.title2)
+                                Text("20℃")
+                            }
+                            .padding(.vertical, 10)
+                            .frame(width: 90)
+                            .background(.gray)
+                            .cornerRadius(8)
                         }
-                        .padding(.all, 8)
+                        .padding(.vertical, 10)
                     }
                 }
 
                 Section("10 day forecast") {
-                    Text("Today")
-                    Text("Sunday")
-                    Text("Today")
-                    Text("Sunday")
-                    Text("Today")
-                    Text("Sunday")
-                    Text("Today")
-                    Text("Sunday")
-                    Text("Today")
+                    HStack {
+                        Text("Today")
+                            .font(.callout)
+                            .padding(.vertical, 10)
+                            .frame(maxWidth: 100, alignment: .leading)
+                            .background(.white)
+                        Spacer()
+                        Image(systemName: "cloud")
+                            .font(.title2)
+                        Spacer()
+                        Text("8℃ / 20℃")
+                            .frame(maxWidth: 120, alignment: .trailing)
+                            .background(.white)
+                    }
+
+                    HStack {
+                        Text("Sunday")
+                            .font(.callout)
+                            .padding(.vertical, 10)
+                            .frame(maxWidth: 100, alignment: .leading)
+                            .background(.white)
+                        Spacer()
+                        Image(systemName: "cloud")
+                            .font(.title2)
+                        Spacer()
+                        Text("18℃ / 20℃")
+                            .frame(maxWidth: 120, alignment: .trailing)
+                            .background(.white)
+                    }
+
+                    HStack {
+                        Text("Saturday")
+                            .font(.callout)
+                            .padding(.vertical, 10)
+                            .frame(maxWidth: 100, alignment: .leading)
+                            .background(.white)
+                        Spacer()
+                        Image(systemName: "cloud")
+                            .font(.title2)
+                        Spacer()
+                        Text("1℃ / 2℃")
+                            .frame(maxWidth: 120, alignment: .trailing)
+                            .background(.white)
+                    }
                 }
             }
             .background(Color(UIColor.systemGroupedBackground))
