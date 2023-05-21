0b6c925863fadfb65e86417e0b0871f2619384bb
Michał Kucal <michal.kucal@intive.com>
2023-05-21 18:25:11 +0200

    Weather state images

--- WeatherForecast/WeatherForecast/Utilities/Int+Utils.swift
+++ WeatherForecast/WeatherForecast/Utilities/Int+Utils.swift
@@ -9,36 +9,34 @@ import Foundation
 
 extension Int {
 
-    // TODO:
-
     var weatherStateData: (iconName: String?, description: String?)? {
         switch self {
         case 0:
-            return ("cloud", NSLocalizedString("Clear sky", comment: ""))
+            return ("sun.max", NSLocalizedString("Clear sky", comment: ""))
         case 1, 2, 3:
             return ("cloud", NSLocalizedString("Cloudy", comment: ""))
         case 45, 48:
-            return ("cloud", NSLocalizedString("Fog", comment: ""))
+            return ("cloud.fog", NSLocalizedString("Fog", comment: ""))
         case 51, 53, 55:
-            return ("cloud", NSLocalizedString("Drizzle", comment: ""))
+            return ("cloud.drizzle", NSLocalizedString("Drizzle", comment: ""))
         case 56, 57:
-            return ("cloud", NSLocalizedString("Freezing drizzle", comment: ""))
+            return ("cloud.drizzle", NSLocalizedString("Freezing drizzle", comment: ""))
         case 61, 63, 65:
-            return ("cloud", NSLocalizedString("Rain", comment: ""))
+            return ("cloud.rain", NSLocalizedString("Rain", comment: ""))
         case 66, 67:
-            return ("cloud", NSLocalizedString("Freezing rain", comment: ""))
+            return ("cloud.rain", NSLocalizedString("Freezing rain", comment: ""))
         case 71, 73, 75:
-            return ("cloud", NSLocalizedString("Snow fall", comment: ""))
+            return ("cloud.snow", NSLocalizedString("Snow fall", comment: ""))
         case 77:
-            return ("cloud", NSLocalizedString("Snow grains", comment: ""))
+            return ("cloud.snow", NSLocalizedString("Snow grains", comment: ""))
         case 80, 81, 82:
-            return ("cloud", NSLocalizedString("Rain showers", comment: ""))
+            return ("cloud.rain", NSLocalizedString("Rain showers", comment: ""))
         case 85, 86:
-            return ("cloud", NSLocalizedString("Snow showers", comment: ""))
+            return ("cloud.snow", NSLocalizedString("Snow showers", comment: ""))
         case 95:
-            return ("cloud", NSLocalizedString("Thunderstorm", comment: ""))
+            return ("cloud.bolt", NSLocalizedString("Thunderstorm", comment: ""))
         case 96, 99:
-            return ("cloud", NSLocalizedString("Thunderstorm", comment: ""))
+            return ("cloud.bolt", NSLocalizedString("Thunderstorm", comment: ""))
         default:
             return nil
         }
